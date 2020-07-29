a9cc08fb3d8535f72f759f647e631b8e9029bc42
agunghimura <gung.d.himura@gmail.com>
2020-07-29 13:39:46 +0800

    Add view and font awesome

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -4,12 +4,41 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
-                    <div class="d-flex align-items-center">
-                    <h1>{{ $question->title }}</h1>
-                </div>
                 <div class="card-body">
-                    {!! $question->body_html !!}
+                    <div class="card-title">
+                        <div class="d-flex align-items-center">
+                        <h1>{{ $question->title }}</h1>
+                    </div>
+                    <hr>
+                    <div class="media">
+                        <div class="d-flex flex-column vote-controls">
+                            <a title="This question is userfull" class="vote-up">
+                                <i class="fas fa-caret-up fa-2x"></i>
+                            </a>
+                            <span class="votes-count">123</span>
+                            <a title="This question is not useful" class="vote-down off">
+                                <i class="fas fa-caret-down fa-2x"></i>
+                            </a>
+                            <a title="Click to mark as favorite question (click again to undo)" class="favorite mt-2 favorited">
+                                <i class="fas fa-star fa"></i>
+                                <span class="favorites-count">456</span>
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            {!! $question->body_html !!}
+                            <div class="float-right">
+                                    <span class="text-muted">Answered {{ $question->created_date }}</span>
+                                    <div class="media mt-2">
+                                    <a href="{{ $question->user->url }}" class="pr-2">
+                                        <img src="{{ $question->user->avatar }}">
+                                    </a>
+                                    <div class="media-body">
+                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                    </div>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -25,6 +54,18 @@
                     <hr>
                     @foreach ($question->answers as $answer)
                         <div class="media">
+                            <div class="d-flex flex-column vote-controls">
+                                <a title="This answer is userfull" class="vote-up">
+                                    <i class="fas fa-caret-up fa-2x"></i>
+                                </a>
+                                <span class="votes-count">123</span>
+                                <a title="This answer is not useful" class="vote-down off">
+                                    <i class="fas fa-caret-down fa-2x"></i>
+                                </a>
+                                <a title="Mark this answer as best asnwer" class="vote-accepted mt-2 favorited">
+                                    <i class="fas fa-check fa"></i>
+                                </a>
+                            </div>
                             <div class="media-body">
                                 {!! $answer->body_html !!}
                                 <div class="float-right">
